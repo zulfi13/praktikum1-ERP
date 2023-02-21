5f00ea86b36b2d0a70e2b6a972f1215d8cc81eaa
zulfi13 <203171007@student.polinema.ac.id>
2023-02-21 10:29:24 +0700

    Jobsheet1_ERP

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+{
+    "name"          : "New Addons",
+    "version"          : "1.0",
+    "author"        : "Zulfia Lutfiani",
+    "website"       : "https://www.detik.com/",
+    "category"      : "New Module",
+    "summary"       : "Ini adalah addons baru",
+    "description"   : """
+    |   Latihan membuat addons baru di odoo 1
+    """,
+    "depends"       : ["base", ], 
+    "data"          : [],
+    "demo"          : [],
+    "test"          : [],
+    "image"         : [],
+    "qweb"          : [],
+    "css"           : [],
+    "application"   : True,
+    "installable"   : True,
+    "auto_install"  : False,
+
+}
